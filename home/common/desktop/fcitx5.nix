8c759c366491757c7f89a5a869358df30a36271a
Fuurin08 <zdf1205654975@gmail.com>
2023-01-09 19:42:05 +0800

    add input method: mozc(japanese)

--- home/common/desktop/fcitx5.nix
+++ home/common/desktop/fcitx5.nix
@@ -2,11 +2,12 @@
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx.engines = with pkgs.fcitx-engines; [ rime ];
+    fcitx.engines = with pkgs.fcitx-engines; [ rime mozc ];
     #fcitx5.enableRimeData = true;
     fcitx5.addons = with pkgs; [
       fcitx5-rime
       fcitx5-chinese-addons
+      fcitx5-mozc
     ];
   };
   home.packages = with pkgs; [
@@ -14,6 +15,5 @@
     fcitx5-lua
     libsForQt5.fcitx5-qt
     fcitx5-configtool
-    librime
   ];
 }
