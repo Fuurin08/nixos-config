c8392aef1f9d44b710b318ac5862abef1a2d0a5c
Fuurin08 <zdf1205654975@gmail.com>
2024-07-16 02:41:13 +0800

    use pipewire

--- hosts/laptop/system.nix
+++ hosts/laptop/system.nix
@@ -32,8 +32,18 @@
   networking.firewall.enable = false;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
   nix.settings = {
     builders-use-substitutes = true;
