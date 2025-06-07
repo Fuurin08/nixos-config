9d92e0a740920ae298185238c74d0ced989ed936
Fuurin08 <zdf1205654975@gmail.com>
2025-06-07 23:47:47 +0800

    game configuration
    steam configuration
    
    gamemode configuration

--- modules/hardware/amdgpu.nix
+++ modules/hardware/amdgpu.nix
@@ -1,6 +1,19 @@
 { ... }:
 
 {
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    amdgpu.amdvlk = {
+      enable = true;
+      support32Bit.enable = true;
+    };
+  };
+
+  hardware.amdgpu.opencl.enable = true;
+
   boot.initrd.kernelModules = [ "amdgpu" ];
 
   services.xserver.videoDrivers = [ "amdgpu" ];
