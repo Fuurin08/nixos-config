3afaf0185c03730d51ce5a024b2e70ec05356789
Fuurin08 <zdf1205654975@gmail.com>
2025-09-03 19:15:25 +0800

    niri/xwayland support

--- home/linux/graphic/niri/services/xwayland.nix
+++ home/linux/graphic/niri/services/xwayland.nix
@@ -8,21 +8,21 @@
     pkgs.xwayland-satellite
   ];
 
-  systemd.user.services.xwayland-satellite = {
-    Unit = {
-      Description = "XWayland Satellite";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
-      Requisite = [ "graphical-session.target" ];
-    };
-    Install = {
-      WantedBy = [ "niri.service" ];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite :0";
-      NotifyAccess = "all";
-      Restart = "on-failure";
-    };
-  };
+  # systemd.user.services.xwayland-satellite = {
+  #   Unit = {
+  #     Description = "XWayland Satellite";
+  #     PartOf = [ "graphical-session.target" ];
+  #     After = [ "graphical-session.target" ];
+  #     Requisite = [ "graphical-session.target" ];
+  #   };
+  #   Install = {
+  #     WantedBy = [ "niri.service" ];
+  #   };
+  #   Service = {
+  #     Type = "simple";
+  #     ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite :0";
+  #     NotifyAccess = "all";
+  #     Restart = "on-failure";
+  #   };
+  # };
 }
