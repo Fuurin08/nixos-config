0fbf10c25b956f59f376bc1bcfe2dce2920fc881
Fuurin08 <zdf1205654975@gmail.com>
2025-11-11 19:38:34 +0800

    modify ssh settings

--- home/users/fuurin.nix
+++ home/users/fuurin.nix
@@ -40,12 +40,20 @@ in
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host github.com
-          Hostname ssh.github.com
-          Port 443
-          User git
-    '';
+    matchBlocks."*" = {
+      compression = true;
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
+    matchBlocks = {
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+        identitiesOnly = true;
+      };
+    };
   };
 
   home = {
