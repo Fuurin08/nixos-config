bc0ae15a98a2bc7edf029b6c9d00860d3ecfacaa
Fuurin08 <zdf1205654975@gmail.com>
2025-11-17 14:13:30 +0800

    lutris

--- home/common/game/default.nix
+++ home/common/game/default.nix
@@ -1,10 +1,14 @@
 {
   pkgs,
+  osConfig,
   ...
 }:
 
 {
   home.packages = with pkgs; [
+    #A Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more.
+    mangohud
+
     # Lossless Scaling
     lsfg-vk
     lsfg-vk-ui
@@ -12,4 +16,21 @@
     # MineCraft launcher
     prismlauncher
   ];
+
+  programs.lutris = {
+    enable = true;
+    steamPackage = osConfig.programs.steam.package;
+    defaultWinePackage = pkgs.proton-ge-bin;
+    protonPackages = [ pkgs.proton-ge-bin ];
+    winePackages = with pkgs; [
+      wineWow64Packages.full
+      wineWowPackages.stagingFull
+    ];
+    extraPackages = with pkgs; [
+      winetricks
+      mangohud
+      gamescope
+      gamemode
+    ];
+  };
 }
